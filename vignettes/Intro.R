4d2b4e43ec77524409f7eb8d4fe93ac9a15a9e57
valeriyafilipova <valeriya.filipova@usn.no>
2018-05-05 10:16:45 +0100

    updated documentation

--- vignettes/Intro.R
+++ vignettes/Intro.R
@@ -58,14 +58,14 @@ print(gincon$plot1)
 ## ----echo=TRUE, fig.height=5, fig.width=5, warning=FALSE,message=FALSE----
  h=simulateP(Nsim=Nsim,durt=24,distfunc="GP",hyet=h,TempSeq=h1,Pexp=d$par,writeResults=FALSE,PDFplots=FALSE)
 
-## ------------------------------------------------------------------------
+## ----message=FALSE, warning=FALSE----------------------------------------
 library(hydroGOF)
-
+#Prepare data
 b1=b[[1]]
 p1$date=as.Date(p1$date,format="%d/%m/%Y")
 b2=match(as.character(b1$date+1),as.character(p1$date))
 data(h1)
-#extrct Precipitation and temperature values
+#extract Precipitation and temperature values
 p_ev=lapply(1:length(b2), function(x) p1[(b2[x]-42):(b2[x]+5),] )
 
 b3=match(as.character(b1$date+1),as.character(as.Date(Qh$Date)))
@@ -75,19 +75,20 @@ b4=match(as.character(b1$date+1),as.character(as.Date(h1$date)))
 t_ev=lapply(1:length(b2), function(x) h1[(b4[x]-42):(b4[x]+5),] )
 PT_ev=lapply(1:length(b2), function(x) cbind(p_ev[[x]][3],t_ev[[x]][3]))
 # 
-xM=sapply(1:length(b2),function(x) max(as.numeric(PT_ev[[x]][[1]])))
+bn=data.frame(b1[17,2:4])
+
 #then take the medium of all values
   #in this case the model performance is poor
 fn1=function(par,...){
-qsim=PQRUT(int1=b1[11,2:4],tm1 = PT_ev[[11]],kd=3,durt =48,param.station =c(0.1138291,0.028429,19.85680),Area1 = 157, snpSpt=0.3,ttsnow=-3,Tmax=0.5,Tmin=0.5 )
-KGE1=-KGE(qsim[[1]],q_ev[[11]][,2])
+qsim=PQRUT(int1=bn,tm1 = PT_ev[[17]],kd=3,durt =48,param.station =c(0.1,0.04,25),Area1 = 157, snpSpt=0.3,ttsnow=-3,Tmax=0.5,Tmin=0.5 )
+KGE1=-KGE(qsim[[1]],q_ev[[17]][,2])
 
 return(KGE1)
 }
 par1=optim(par = c(0.1,0.04,25),fn = fn1,method = "L-BFGS-B",lower=c(0.01,0.01,1),upper = c(0.5,0.5,100))
 # 
 # 
-qsim=PQRUT(int1=b1[11,2:4],tm1 = PT_ev[[11]],kd=3,durt =48,param.station =c(0.1138291,0.028429,19.85680),Area1 = 157, snpSpt=0.3,ttsnow=-3,Tmax=0.5,Tmin=0.5)
+qsim=PQRUT(int1=bn,tm1 = PT_ev[[17]],kd=3,durt =48,param.station =par1$par,Area1 = 157, snpSpt=0.3,ttsnow=-3,Tmax=0.5,Tmin=0.5)
 plot(q_ev[[1]][,2],type="l")
 points(qsim[[1]],type="l",col="green")
 
